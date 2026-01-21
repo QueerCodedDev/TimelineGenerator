dbd2285591fd64ed7c4e8a43a2f881239ebe270a
QueerCodedDev <120541616+QueerCodedDev@users.noreply.github.com>
2026-01-21 20:47:51 +0000

    Data for Leverage should be all entered. Redemption, Quest for Spear, King Solomon's Mines, Judas Chalice, The Librarians, Next Chapter, Psych, Psych The Movie, Lassie Come Home, This is GUs, Supernatural, and Mentalist are all needed still.

---
+++ util/convert_csv_to_json.py
@@ -0,0 +1,13 @@
+import csv
+
+data = []
+
+with open('/workspaces/TimelineGenerator/shows_timeline/leverage_data.csv') as file:
+    csv_data = csv.reader(file)
+    for lines in csv_data:
+        print('{')
+        print(f'    "season": "{lines[0]}",')
+        print(f'    "episode": "{lines[1]}",')
+        print(f'    "title": "The {lines[2]} Job",')
+        print(f'    "air_date": "{lines[3]}"')
+        print('},')
