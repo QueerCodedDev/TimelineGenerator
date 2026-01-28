94bb2b587e5ae2764d83089ca3bf2fbb055346ae
QueerCodedDev <120541616+QueerCodedDev@users.noreply.github.com>
2026-01-28 18:13:25 +0000

    Attempting to add month lines to timeline background

--- sketch.js
+++ sketch.js
@@ -34,15 +34,30 @@ function setup_timeline_background() {
         y1 = i*timeline_row_height;
         x2 = canvas_width;
         y2 = i * timeline_row_height;
+
         line(x1, y1, x2, y2);
     }
 
     // draw lines to show the year marks
     for (let i = 0; i < total_years_spanned_cieling; i++) {
-        x1 = i * timeline_row_length
-        y1 = 0
-        x2 = i * timeline_row_length
-        y2 = canvas_height
+        x1 = i * timeline_row_length;
+        y1 = 0;
+        x2 = i * timeline_row_length;
+        y2 = canvas_height;
+
+        line(x1, y1, x2, y2);
+    }
+
+    // set stroke alpha so that month lines are lighter than year lines
+    stroke(0, 0, 0, 50);
+
+    // draw lines to show the month marks
+    for (let i = 0; i < total_years_spanned_cieling * 12; i++) {
+        x1 = i * timeline_row_length / 12;
+        y1 = 0;
+        x2 = i * timeline_row_length / 12;
+        y2 = canvas_height;
+
         line(x1, y1, x2, y2);
     }
 }
