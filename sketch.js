432c3b178b31efe999ad7d269fda450bb1d7df95
QueerCodedDev <120541616+QueerCodedDev@users.noreply.github.com>
2026-01-28 16:28:37 +0000

    testing json to array

--- sketch.js
+++ sketch.js
@@ -1,10 +1,13 @@
+let series_data_json;
+let series_data_arr;
+
+function preload() {
+    series_data_json = loadJSON('res/series_data.json');
+    series_data_arr = series_data_json[0];
+}
+
 function setup() {
     createCanvas(400,400);
-    let series_data_json = loadJSON('res/series_data.json');
-    let series_data_arr = []
-    for(let d of series_data_json) {
-      series_data_arr.push(d);
-    }
     print(series_data_arr);
 }
 
