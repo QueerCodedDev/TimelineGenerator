fca7bd65a0ffe610f46485ce26597a7ec1b088fd
QueerCodedDev <120541616+QueerCodedDev@users.noreply.github.com>
2026-01-28 16:09:27 +0000

    testing parsing of the json object

--- sketch.js
+++ sketch.js
@@ -1,10 +1,10 @@
 function setup() {
     createCanvas(400,400);
-    example = loadJSON('res/series_data.json');
+    example = loadJSON('res/series_data.json').parse();
     console.log(example)
     console.log(example.length())
 }
 
 function draw() {
-  background(255,0,0);
+  background(255,50,0);
 }
