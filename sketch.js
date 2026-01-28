aa63af061e497fd121eedd6e04769c9d5cb6d86d
QueerCodedDev <120541616+QueerCodedDev@users.noreply.github.com>
2026-01-28 16:50:19 +0000

    testing row spacing

--- sketch.js
+++ sketch.js
@@ -1,3 +1,6 @@
+let timeline_row_height = 100;
+let timeline_row_length = 200;
+let total_years_spanned_cieling = 25;
 let series_data_json;
 let series_data_arr;
 
@@ -6,11 +9,17 @@ function preload() {
 }
 
 function setup() {
-    createCanvas(400,400);
     series_data_arr = series_data_json.shows;
-    print(series_data_arr);
+
+    createCanvas(timeline_row_height*series_data_arr.length(), timeline_row_length*total_years_spanned_cieling);
+
+    
 }
 
 function draw() {
   background(255,50,0);
+  for (let i = 0; i < series_data_arr.length; i++) {
+      line(0, i*timeline_row_height, width, i*timeline_row_height);
+    }
+    noLoop();
 }
