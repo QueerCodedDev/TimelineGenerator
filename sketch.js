a9e87f70e44b0c2bb1a62659fe3baeb96ba399c5
QueerCodedDev <120541616+QueerCodedDev@users.noreply.github.com>
2026-01-28 16:35:50 +0000

    maybe fixed converting json

--- sketch.js
+++ sketch.js
@@ -3,12 +3,11 @@ let series_data_arr;
 
 function preload() {
     series_data_json = loadJSON('res/series_data.json');
-    series_data_arr = series_data_json.shows;
 }
 
 function setup() {
     createCanvas(400,400);
-    print(series_data_json);
+    series_data_arr = series_data_json.shows;
     print(series_data_arr);
 }
 
