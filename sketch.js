6f699f962dd1f87fd26170e7dde9fe128e0fb7bb
QueerCodedDev <120541616+QueerCodedDev@users.noreply.github.com>
2026-01-28 17:36:39 +0000

    adjusting timeline design

--- sketch.js
+++ sketch.js
@@ -12,17 +12,20 @@ function preload() {
 
 function setup() {
     series_data_arr = series_data_json.shows;
-    canvas_height = timeline_row_height*series_data_arr.length;
-    canvas_width = timeline_row_length*total_years_spanned_cieling;
+    canvas_height = timeline_row_height * series_data_arr.length;
+    canvas_width = timeline_row_length * total_years_spanned_cieling;
+
     createCanvas(canvas_width, canvas_height);
 
-    
 }
 
 function draw() {
-  background(255,50,0);
-  for (let i = 0; i < series_data_arr.length; i++) {
-      line(0, i*timeline_row_height, width, i*timeline_row_height);
+    background(255,50,0);
+    for (let i = 0; i < series_data_arr.length; i++) {
+        line(0, i*timeline_row_height, width, i*timeline_row_height);
+    }
+    for (let i = 0; i < total_years_spanned_cieling; i++) {
+        line(i * timeline_row_length, 100, i * timeline_row_length, i * timeline_row_length + 50);
     }
     noLoop();
 }
