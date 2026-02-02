38e26ed7f7d6f747a0a37f2b95433c07012ade52
QueerCodedDev <120541616+QueerCodedDev@users.noreply.github.com>
2026-02-02 16:15:40 +0000

    Adjusting text placement

--- classes/Entry.js
+++ classes/Entry.js
@@ -14,7 +14,7 @@ class Entry {
     render(x, y) {
         // function variable placement reference: rect(x, y, length, height)
         rect(x, y, this.render_length, timeline_row_height);
-        text(this.name, x + 25, y - 25);
+        text(this.name, x + 25, y + 25);
     }
 
     calc_entry_length() {
