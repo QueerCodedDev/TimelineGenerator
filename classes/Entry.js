40d2da88bd81e93d85c8bb18f972a88fd015adb5
QueerCodedDev <120541616+QueerCodedDev@users.noreply.github.com>
2026-02-02 16:38:31 +0000

    Added switch statement to determine color for entry, as well as added a line at the start of render() to set the fill to the correct color.

--- classes/Entry.js
+++ classes/Entry.js
@@ -6,16 +6,39 @@ class Entry {
         this.start = data.start;
         this.end   = data.end;
 
+        this.fill_color    = this.determine_entry_color();
         this.render_length = this.calc_entry_length();
 
-            
     }
 
     render(x, y) {
         // function variable placement reference: rect(x, y, length, height)
+        fill(this.fill_color);
         rect(x, y, this.render_length, timeline_row_height);
         textSize(25);
         text(this.name, x + 25, y + timeline_row_height / 2);
+    }
+
+    determine_entry_color() {
+        switch(this.uni) {
+            case 'lib':
+                this.fill_color = "#F5D327"
+                break;
+            case 'lev':
+                this.fill_color = "#27B0F5"
+                break;
+            case 'spn':
+                this.fill_color = "#C223BD"
+                break;
+            case 'psy':
+                this.fill_color = "#23C227"
+                break;
+            case 'men':
+                this.fill_color = "#C22823"
+                break;
+            default:
+                this.fill_color = "#FFFFFF"
+        }
     }
 
     calc_entry_length() {
