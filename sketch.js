b9cd0c7e95a17a6cb3373da1e13f18ac1c0a0b23
QueerCodedDev <120541616+QueerCodedDev@users.noreply.github.com>
2026-02-06 19:08:09 +0000

    Refactored code to accomodate the possibility of multiple timeline modes

--- sketch.js
+++ sketch.js
@@ -3,25 +3,41 @@ let timeline_row_length = 365;
 let canvas_width;
 let canvas_height;
 let series_data_json;
-let SeriesEntryManager;
+let episode_data_json;
+let ActiveEntryManager;
+let EpisodeEntryManager;
+let ActiveEntryManager;
+
+let TIMELINE_OPTIONS = {
+    'SERIES': 'series',
+    'EPISODE': 'episode'
+} 
+
+let TIMELINE_CHOICE = TIMELINE_OPTIONS.SERIES;
 
 function preload() {
     series_data_json = loadJSON('res/series_data.json');
+    episode_data_json = loadJSON('res/shows_data.json')
 }
 
 function setup() {
-    SeriesEntryManager = new EntryManager(series_data_json.media);
-    canvas_height = timeline_row_height * SeriesEntryManager.entries_arr.length;
-    canvas_width  = timeline_row_length * SeriesEntryManager.total_duration;
+    if (TIMELINE_CHOICE == TIMELINE_OPTIONS.SERIES) {
+        ActiveEntryManager = new EntryManager(series_data_json.media);
+    } else if (TIMELINE_CHOICE == TIMELINE_OPTIONS.EPISODE) {
+        ActiveEntryManager = new EntryManager(episode_data_json);
+    }
 
+    canvas_height = timeline_row_height * ActiveEntryManager.entries_arr.length;
+    canvas_width  = timeline_row_length * ActiveEntryManager.total_duration;
+    
     createCanvas(canvas_width, canvas_height);
 }
 
 function draw() {
     background('black');
-    setup_timeline_background(SeriesEntryManager);
+    setup_timeline_background(ActiveEntryManager);
 
-    SeriesEntryManager.render_entries();
+    ActiveEntryManager.render_entries();
 
     noLoop();
 }
