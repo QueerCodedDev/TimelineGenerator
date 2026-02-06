da851b108230084c274f8961bc4ec08a1a63e4cb
QueerCodedDev <120541616+QueerCodedDev@users.noreply.github.com>
2026-02-06 17:48:14 +0000

    Added commentation, and made a few changes to the way getting the timeline range works

--- classes/EntryManager.js
+++ classes/EntryManager.js
@@ -1,10 +1,16 @@
 class EntryManager {
-    constructor(entries_data, starting_year) {
+    constructor(entries_data) {
+        // Array of all entries involved
         this.entries_arr = this.parse_entries_data(entries_data);
-        this.starting_year = this.calc_starting_year();
-            
+        // Get the earliest year present in data
+        // as well as the latest year present in data
+        this.start_end_year_data = this.get_start_and_end_year();
+        // Parsing data from start_end_year_data for readibility
+        this.starting_year = this.start_end_year_data.start_year;
+        this.ending_year = this.start_end_year_data.end_year;
     }
 
+    // Function for parsing data and using it for creating Entries
     parse_entries_data(data) {
         let temp_arr = [];
         for (let d of data) {
@@ -14,22 +20,36 @@ class EntryManager {
         return temp_arr;
     }
 
-    calc_starting_year() {
+    /**
+    * Function for determining the earliest and latest years involved in the timeline
+    */
+    get_start_and_end_year() {
+        // Set earliest_year and latest_year to theoritcal highest and lowest respectfully
         let earliest_year = 9999;
+        let latest_year = 0;
+        // Get year from start and year from end, compare to existing.
+        // Replace earliest_year and latest_year with new values as needed.
         for (let d of this.entries_arr) {
-            let temp_year = d.start.split('-')[2];
-            if (temp_year < earliest_year) earliest_year = temp_year;
+            let temp_year_start = d.start.split('-')[2];
+            if (temp_year_start < earliest_year) earliest_year = temp_year_start;
+
+            let temp_year_end = d.end.split('-')[2];
+            if (temp_year_end > latest_year) latest_year = temp_year_end;
         }
 
-        console.log(earliest_year);
-        return earliest_year;
+        // returning start_year and end_year as JSON for ease
+        return { 'start_year': earliest_year, 'end_year': latest_year };
     }
 
+    /**
+     * Function for rendering all the Entries that are included in this manager.
+     * Calls render for each Entry, and the Entry handles the rest.
+     */
     render_entries() {
         let y = 0;
         for (let e of this.entries_arr) {
             // calculate the distance from the start of the timeline to the start of the entry
-            let x = calc_dist_as_days('01-01-' + starting_year, e.start);
+            let x = calc_dist_as_days('01-01-' + this.starting_year, e.start);
             e.render(x, y * timeline_row_height);
             y++;
         }
