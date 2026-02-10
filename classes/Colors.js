cba7bc27878c23f2c4ac619596424627d8529191
QueerCodedDev <120541616+QueerCodedDev@users.noreply.github.com>
2026-02-10 22:07:11 +0000

    Changing the way colors are stored and determined

---
+++ classes/Colors.js
@@ -0,0 +1,9 @@
+class Colors {
+    static colors = {
+        'lib': color(245, 211,  39),
+        'lev': color( 39, 176, 245),
+        'spn': color(194,  35, 189),
+        'psy': color( 35, 194,  39),
+        'men': color(194,  40,  35)
+        }
+}
--- classes/Entry.js
+++ classes/Entry.js
@@ -7,7 +7,7 @@ class Entry {
         this.end   = data.end;
         this.eps   = data.episodes;
 
-        this.fill_color    = this.determine_entry_color();
+        this.fill_color    = Colors.colors[this.uni]
         this.render_length = this.calc_entry_length(this.start, this.end);
         
         this.seasons = this.get_seasons_data();
