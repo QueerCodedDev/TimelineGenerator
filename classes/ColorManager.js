b5c1905d9096085ba5ff02bfd77504f169e22f57
QueerCodedDev <120541616+QueerCodedDev@users.noreply.github.com>
2026-02-11 16:06:56 +0000

    Changed color definitions to hexcode

--- classes/ColorManager.js
+++ classes/ColorManager.js
@@ -1,9 +1,9 @@
 class ColorManager {
     static colors = {
-        'lib': 'F5D327',
-        'lev': '27B0F5',
-        'spn': 'C223BD',
-        'psy': '23C227',
-        'men': 'C22823'
+        'lib': '#F5D327',
+        'lev': '#27B0F5',
+        'spn': '#C223BD',
+        'psy': '#23C227',
+        'men': '#C22823'
         }
 }
