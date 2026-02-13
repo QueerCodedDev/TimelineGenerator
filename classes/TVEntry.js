44db8c81cbe2daaaba8b79c054f6a254008c9cd3
QueerCodedDev <120541616+QueerCodedDev@users.noreply.github.com>
2026-02-13 17:27:17 +0000

    Added render() to TVEntry also

--- classes/TVEntry.js
+++ classes/TVEntry.js
@@ -4,4 +4,14 @@ class TVEntry extends MediaEntry {
         this.episodes;
         this.seasons;
     }
+
+    render() {
+        fill(this.fill_color);
+        rect(
+            this.bounds.x1,
+            this.bounds.y1,
+            this.bounds.length,
+            this.bounds.height
+        )
+    };
 }
