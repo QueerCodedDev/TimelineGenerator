5656c66834b529b1a3b879d7b2082a2e47513949
QueerCodedDev <120541616+QueerCodedDev@users.noreply.github.com>
2026-02-13 19:40:03 +0000

    Added render_boarder to render_popup to adjust text placement

--- classes/MovieEntry.js
+++ classes/MovieEntry.js
@@ -8,12 +8,13 @@ class MovieEntry extends MediaEntry {
     }
 
     render_popup(mx) {
+        let render_border = 2;
         fill(this.fill_color);
         rect(mx, this.bounds.y2, textWidth(this.popup_text), textAscent());
         fill('black');
         textSize(20);
         textAlign(LEFT, TOP);
         noStroke();
-        text(this.popup_text, mx, this.bounds.y2);
+        text(this.popup_text, mx + render_border, this.bounds.y2 + render_border);
     }
 }
