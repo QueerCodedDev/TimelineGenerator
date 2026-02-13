995e59311cddb080df83ecc8b63cd93bad23e516
QueerCodedDev <120541616+QueerCodedDev@users.noreply.github.com>
2026-02-13 17:48:02 +0000

    Added entry text rendering for debugging

--- classes/MediaEntry.js
+++ classes/MediaEntry.js
@@ -29,4 +29,22 @@ class MediaEntry {
 
         this.bounds = bounds;
     }
+
+    render() {
+        fill(this.fill_color);
+        rect(
+            this.bounds.x1,
+            this.bounds.y1,
+            this.bounds.length,
+            this.bounds.height
+        )
+
+        fill(255);
+        textSize(25);
+        textWrap(WORD);
+        textAlign(LEFT, CENTER);
+        stroke('black');
+        strokeWeight(2);
+        text(this.name, this.bounds.x1 + 25, this.bounds.y1 + timeline_row_height / 2, this.render_length);
+    };
 }
