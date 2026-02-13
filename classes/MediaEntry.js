b72f57711990283f5bad48469abf953f2a352c82
QueerCodedDev <120541616+QueerCodedDev@users.noreply.github.com>
2026-02-13 17:31:29 +0000

    Moved calc_bounds to MediaEntry.

--- classes/MediaEntry.js
+++ classes/MediaEntry.js
@@ -8,5 +8,25 @@ class MediaEntry {
         this.bounds;
     }
 
-    calc_bounds(start){};
+    calc_bounds(start) {
+        let bounds = {
+            'x1': 0,
+            'y1': 0,
+            'x2': 0,
+            'y2': 0,
+            'length': 0,
+            'height': 0,
+        }
+
+        bounds.x1 = calc_dist_as_days(start, this.start);
+        bounds.y1 = activeEntryManager.universe_tags.indexOf(this.universe);
+        bounds.x2 = calc_dist_as_days(bounds.x1, this.end);
+        bounds.y2 = bounds.y1 + timeline_row_height;
+        bounds.length = bounds.x2 - bounds.x1;
+        bounds.height = bounds.y2 - bounds.y1;
+
+        console.log(bounds)
+
+        return bounds;
+    }
 }
