09d039be04923f5b7ac8d221247d2cd612665bff
QueerCodedDev <120541616+QueerCodedDev@users.noreply.github.com>
2026-02-13 17:46:02 +0000

    Added entry text rendering for debugging

--- classes/MovieEntry.js
+++ classes/MovieEntry.js
@@ -13,5 +13,13 @@ class MovieEntry extends MediaEntry {
             this.bounds.length,
             this.bounds.height
         )
+
+        fill(255);
+        textSize(25);
+        textWrap(WORD);
+        textAlign(LEFT, CENTER);
+        stroke('black');
+        strokeWeight(2);
+        text(this.name, this.bounds.x1 + 25, this.bounds.y1 + timeline_row_height / 2, this.render_length);
     };
 }
