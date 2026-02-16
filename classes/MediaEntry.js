21c2de3212bb458bce153589d18283e1a06deb52
QueerCodedDev <120541616+QueerCodedDev@users.noreply.github.com>
2026-02-16 21:05:21 +0000

    Working on styling the entries and adding a border based on universe color

--- classes/MediaEntry.js
+++ classes/MediaEntry.js
@@ -4,7 +4,7 @@ class MediaEntry {
         this.universe = data.universe;
         this.air_date = data.air_date;
         this.color    = ColorManager.colors[this.universe];
-        this.view     = createDiv().addClass('view').attribute('border-color', this.color).hide();
+        this.view     = createDiv().addClass('view').attribute('border', `2px solid ${this.color}`).hide();
     }
 
     render() {
