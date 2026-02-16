c101adbab3773b4a3ec8a2a0813674e3c4c2dc30
QueerCodedDev <120541616+QueerCodedDev@users.noreply.github.com>
2026-02-16 18:00:35 +0000

    Removed some test code

--- sketch.js
+++ sketch.js
@@ -6,12 +6,9 @@ function preload() {
 }
 
 function setup() {
+    // Create an EntryManager using the json data provided
     entryManager = new EntryManager (media_data_json.media)
-    let i = 1;
-    for (let md of entryManager.entries_arr) {
-        createDiv(`${i}. ${md.air_date} | Name: ${md.name} | Universe: ${md.universe}`);
-        i++;
-    }
+
 }
 
 function draw() {
