fd093f550977659051788b5248b7b4d8b9c60a33
QueerCodedDev <120541616+QueerCodedDev@users.noreply.github.com>
2026-02-16 17:48:47 +0000

    Documentation/Comments

--- classes/EntryManager.js
+++ classes/EntryManager.js
@@ -26,22 +26,29 @@ class EntryManager {
     // Sorting Entries by air_date
     sort(arr) {
         let sorted_arr = [];
+
+        // While there is at least 1 value that needs to be sorted
         while (arr.length >= 1) {
+            // Let the first item in array be the minimum
             let min = arr[0];
+
+            // For each item in the array
             for (let i = 0; i < arr.length; i++) {
+                // Convert min_date and checking into Dates for comparison
                 let min_date = new Date(min.air_date);
                 let checking = new Date(arr[i].air_date);
+                // If existing min is greater than what is being checked,
+                // replace existing min with what is being checked
                 if (min_date > checking) {
                     min = arr[i];
                 }
             }
+
+            // Add current min to the sorted array
             sorted_arr.push(min);
+            // Remove added min from the array of items that still need sorting
             arr.splice(arr.indexOf(min), 1);
         }
-
-        // Push what should be the last element left in the array, to the end of the sorted array
-        //sorted_arr.push(arr[0]);
-        console.log(arr)
 
         return sorted_arr;
     }
