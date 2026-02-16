40970b79add6a266c615a6f50afce307feeb9e50
QueerCodedDev <120541616+QueerCodedDev@users.noreply.github.com>
2026-02-16 16:56:34 +0000

    Changed div creation to happen after EntryManager and entries are made

--- sketch.js
+++ sketch.js
@@ -1,12 +1,13 @@
 let media_data_json;
+let entryManager;
 
 function preload() {
     media_data_json = loadJSON('res/individual_data.json')
 }
 
 function setup() {
-    
-    for (let md of media_data_json.media) {
+    entryManager = new EntryManager (media_data_json.media)
+    for (let md of entryManager.entries_arr) {
         createDiv(`${md.air_date} | Name: ${md.name} | Universe: ${md.universe}`);
     }
 }
