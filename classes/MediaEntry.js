bfdaa3a90ca64562748bedca72187562d8f6f942
QueerCodedDev <120541616+QueerCodedDev@users.noreply.github.com>
2026-02-16 20:29:50 +0000

    Testing createDiv functionality to determine how best to write the render portion

--- classes/MediaEntry.js
+++ classes/MediaEntry.js
@@ -4,9 +4,10 @@ class MediaEntry {
         this.universe = data.universe;
         this.air_date = data.air_date;
         this.color    = ColorManager.colors[this.universe];
+        this.view     = createDiv(`${this.name}<br>${this.air_date}`);
     }
 
     render() {
-        createDiv(`${this.name}<br>${this.air_date}`);
+        
     }
 }
