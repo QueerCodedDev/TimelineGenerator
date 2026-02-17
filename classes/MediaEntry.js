d90d0617e9a5895e7343d6d0c9d9ef97220c873e
QueerCodedDev <120541616+QueerCodedDev@users.noreply.github.com>
2026-02-17 21:43:58 +0000

    Changed html class from 'view' to 'entry'

--- classes/MediaEntry.js
+++ classes/MediaEntry.js
@@ -4,6 +4,6 @@ class MediaEntry {
         this.universe = data.universe;
         this.air_date = data.air_date;
         this.color    = ColorManager.colors[this.universe];
-        this.view     = createDiv().addClass('view').attribute('border-color', this.color);
+        this.view     = createDiv().addClass('entry').attribute('border-color', this.color);
     }
 }
