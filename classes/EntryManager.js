bf0cf12169f23586d8946bf8760870404a75dccc
QueerCodedDev <120541616+QueerCodedDev@users.noreply.github.com>
2026-02-21 23:27:21 +0000

    Removed depricated call

--- classes/EntryManager.js
+++ classes/EntryManager.js
@@ -9,7 +9,6 @@ class EntryManager {
         };
         // Array of all entries involved
         this.entries_arr = this.parse_entries_data(entries_data);
-        this.format_entries();
     }
 
     // Parse data, create entries, and then sort entries by air_date
