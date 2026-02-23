3194244b8cf85bb32b055ce9357cbcf6bc4301dc
QueerCodedDev <120541616+QueerCodedDev@users.noreply.github.com>
2026-02-23 21:16:25 +0000

    Removed debugging comment

--- classes/MediaEntry.js
+++ classes/MediaEntry.js
@@ -47,8 +47,6 @@ class MediaEntry {
         this.renderSubtext();
 
         translate(0, this.dims.h * 2);
-
-        if (this.prev && this.next) console.log(`Prev: ${this.prev.air_date} | Curr: ${this.air_date} | Next: ${this.next.air_date}`);
     }
 
     renderRect() {
