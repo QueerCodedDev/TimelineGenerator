7418e56a78fbfa762d5c011afee1767344a7fe72
QueerCodedDev <120541616+QueerCodedDev@users.noreply.github.com>
2026-02-23 21:12:49 +0000

    debugging

--- classes/MediaEntry.js
+++ classes/MediaEntry.js
@@ -48,7 +48,7 @@ class MediaEntry {
 
         translate(0, this.dims.h * 2);
 
-        console.log(`Prev: ${this.prev.air_date? this.prev.air_date : null} | Curr: ${this.air_date} | Next: ${this.next.air_date? this.next.air_date : null}`);
+        if (this.prev && this.next) console.log(`Prev: ${this.prev.air_date} | Curr: ${this.air_date} | Next: ${this.next.air_date}`);
     }
 
     renderRect() {
