3855d584e4dd447c442218367c4bad740ce1a601
QueerCodedDev <120541616+QueerCodedDev@users.noreply.github.com>
2026-02-24 21:08:52 +0000

    pixel density 2

--- sketch.js
+++ sketch.js
@@ -11,7 +11,7 @@ function preload() {
 
 function setup() {
     textFont('Consolas'); //fixed width font
-    pixelDensity(5);
+    pixelDensity(2);
 
     // Create an EntryManager using the json data provided
     entryManager = new EntryManager(media_data_json.media);
