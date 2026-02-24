f28c6ef108dc9bb60b2be2dbcf1c54e9946e50fd
QueerCodedDev <120541616+QueerCodedDev@users.noreply.github.com>
2026-02-24 21:06:01 +0000

    Fixed typo

--- sketch.js
+++ sketch.js
@@ -11,7 +11,7 @@ function preload() {
 
 function setup() {
     textFont('Consolas'); //fixed width font
-    conspixelDensity(5);
+    pixelDensity(5);
 
     // Create an EntryManager using the json data provided
     entryManager = new EntryManager(media_data_json.media);
