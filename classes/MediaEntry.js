e9104a941b42a27f94daef21cb99b6e1a3ba47db
QueerCodedDev <120541616+QueerCodedDev@users.noreply.github.com>
2026-02-24 18:48:54 +0000

    added spacing from center to the entries

--- classes/MediaEntry.js
+++ classes/MediaEntry.js
@@ -67,9 +67,9 @@ class MediaEntry {
         group = before + 1 + after;
 
         if (before == 0 && after == 1) { // JUST left of center
-            this.dims.x = this.dims.w / -2;
+            this.dims.x = (this.dims.w / -2) - 10;
         } else if (before == 1 && after == 0) { // JUST right of center
-            this.dims.x = this.dims.w / 2;
+            this.dims.x = (this.dims.w / 2) + 10;
         } else if (before < after) { // Generally left of center;
             let offset  = (after - before + 1) / 2;
             this.dims.x = offset * (entryManager.entry_stats.max_w / -group / 2);
