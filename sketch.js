301239538703320217a99f380ffdbd4cd5e1a43c
QueerCodedDev <120541616+QueerCodedDev@users.noreply.github.com>
2026-03-03 17:56:13 +0000

    Testing default variables again, and switching back to grouped to verify functionality before going too much futher

--- sketch.js
+++ sketch.js
@@ -14,7 +14,7 @@ function setup() {
     textFont('Consolas'); //fixed width font
 
     // Create an EntryManager using the json data provided
-    entryManager = new EntryManager(media_data_json.media, false, true);
+    entryManager = new EntryManager(media_data_json.media, group=true);
     entryManager.formatEntries();
     
     rectMode(CENTER);
