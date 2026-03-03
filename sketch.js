07d9b4ff69cefa2ff8dab95d6cc9e4a6d317175a
QueerCodedDev <120541616+QueerCodedDev@users.noreply.github.com>
2026-03-03 16:58:41 +0000

    Removed autoscroll attempts

--- sketch.js
+++ sketch.js
@@ -1,18 +1,9 @@
-// No longer save image, just render a smaller canvas and have a search function to 
-// find episodes to save from scrolling. For recording auto scroll with increment. 
-// Only render the entries that would fit to the canvas.
-
 let media_data_json;
 let entryManager;
 let canvasDim = {
     'height': 50000,
     'width':  1000
 };
-
-// Auto scroll attempts
-// let scroll_speed = 0;
-// let auto_scroll  = false;
-// let first_pass   = true;
 
 function preload() {
     media_data_json = loadJSON('res/individual_data.json');
@@ -21,10 +12,9 @@ function preload() {
 function setup() {
     createCanvas(canvasDim.width, canvasDim.height);
     textFont('Consolas'); //fixed width font
-    //pixelDensity(2);
 
     // Create an EntryManager using the json data provided
-    entryManager = new EntryManager(media_data_json.media, false, true); // <- There might be an issue with this dealing with using entryManager vars too early???? not sure. just keeps telling me undefined.
+    entryManager = new EntryManager(media_data_json.media, false, true);
     entryManager.formatEntries();
     
     rectMode(CENTER);
@@ -32,24 +22,9 @@ function setup() {
 }
 
 function draw() {
-    // Autoscroll attempt
-    // if (first_pass) {
-    //     translate(canvasDim.width/2, 0);
-    //     first_pass = false;
-    // }
-
     translate(canvasDim.width/2, 0)
     stroke(100);
     strokeWeight(6);
     line(0, 0, 0, canvasDim.height);
     entryManager.render();
-
-    // Autoscroll attempt
-    // if (auto_scroll) translate(0, scroll_speed);
 }
-
-// Autoscroll attempt
-// function autoScroll(speed) {
-//     auto_scroll = !auto_scroll;
-//     scroll_speed = speed;
-// }
