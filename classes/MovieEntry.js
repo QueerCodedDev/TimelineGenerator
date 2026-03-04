d9975cdd49741fcd94a528e4a9d4ebafe01a5bfe
QueerCodedDev <120541616+QueerCodedDev@users.noreply.github.com>
2026-03-04 05:00:34 +0000

    Deleted

--- classes/MovieEntry.js
+++
@@ -1,8 +0,0 @@
-// is movie entry if episodes == null
-
-class MovieEntry extends MediaEntry {
-    constructor(data) {
-        super(data);
-        this.header = `${this.title}\n${this.air_date}`;
-    }
-}
