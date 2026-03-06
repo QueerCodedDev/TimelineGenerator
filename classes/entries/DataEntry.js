4e1b8ba877da5b3b8ea50313d60c4c52f6dc80ca
QueerCodedDev <120541616+QueerCodedDev@users.noreply.github.com>
2026-03-06 18:57:54 +0000

    Rearranged file structure. Added new files/classes. Started building DataEntry

---
+++ classes/entries/DataEntry.js
@@ -0,0 +1,14 @@
+class DataEntry {
+    constructor(data) {
+        // Get guaranteed data
+        this.name    = data.name;
+        this.title   = data.title;
+        this.date    = data.air_date;
+
+        // Get circumstantial data
+        this.season  = data.season  ? data.season  : null;
+        this.episode = data.episode ? data.episode : null;
+
+        //this.color; <-- Unsure if this var will actually be used or not. 
+    }
+}
