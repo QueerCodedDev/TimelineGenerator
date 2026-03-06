755ce32cc4270864d379f9ff14fa00511f5b86fb
QueerCodedDev <120541616+QueerCodedDev@users.noreply.github.com>
2026-03-06 20:14:14 +0000

    Testing sorting by date, before trying to expand for other sorting methods

--- classes/managers/DataEntryManager.js
+++ classes/managers/DataEntryManager.js
@@ -1,6 +1,7 @@
 class DataEntryManager {
     constructor(dataJSON) {
         this.dataEntries = this.createDataEntries(dataJSON);
+        this.dataEntriesByAirDate = this.sort(this.dataEntries);
     }
 
     createDataEntries(data) {
@@ -10,5 +11,35 @@ class DataEntryManager {
         }
 
         return dataEntries;
+    }
+
+    sort(arr) { // by air date
+        let sortedArr = [];
+
+        // While there is at least 1 item that needs to be sorted
+        while (arr.length >= 1) {
+            // Assume the first value in the array is the minimum
+            let minItem = arr[0];
+
+            // For each item in arr
+            for (let i = 0; i < arr.length; i++) {
+                minItem = this.compare(minItem, arr[i]);
+            }
+
+            sortedArr.push(minItem);
+            arr.splice(arr.indexOf(minItem), 1);
+        }
+
+        return sortedArr;
+    }
+
+    compare(min, cur) {
+        let a = new Date(min.date);
+        let b = new Date(cur.date);
+
+        if (a > b) return b;
+        if (a < b) return a;
+
+        return null;
     }
 }
