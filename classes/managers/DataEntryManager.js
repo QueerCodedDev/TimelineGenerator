6a31903d2e24ac0fff50a931667af0659b19f5d8
QueerCodedDev <120541616+QueerCodedDev@users.noreply.github.com>
2026-03-08 05:42:04 +0000

    Testing by universe only since date might have been messing with it

--- classes/managers/DataEntryManager.js
+++ classes/managers/DataEntryManager.js
@@ -78,7 +78,7 @@ class DataEntryManager {
             case SORT_OPTIONS.DATE:
                 if (min.dateOBJ > cur.dateOBJ) return cur;
             case SORT_OPTIONS.UNIVERSE:
-                if (min.universe > cur.universe && min.dateOBJ > cur.dateOBJ) return cur;
+                if (min.universe > cur.universe) return cur;
             
         }
 
