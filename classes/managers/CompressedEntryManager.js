fbd34c6fdab289c34a275f0966a81b94fef52a47
QueerCodedDev <120541616+QueerCodedDev@users.noreply.github.com>
2026-03-09 18:35:32 +0000

    Testing compression of entries before creating compressionEntries

--- classes/managers/CompressedEntryManager.js
+++ classes/managers/CompressedEntryManager.js
@@ -1,5 +1,20 @@
 class CompressedEntryManager {
-    constructor() {
-        
+    constructor(dataEM) {
+        this.dataEntries = dataEM.dataEntriesByAirDate;
+        this.compEntries = this.compressEntries(this.dataEntries);
+        console.log(this.compEntries);
+    }
+
+    compressEntries(arr) {
+        let compArr = [[arr[0]]]; // Store first entry in array by default
+        for (let i = 1; i < arr.length-1; i++) {
+            if (arr[i].universe == compArr[compArr.length-1].universe) {
+                compArr[compArr.length-1].push(arr[i]);
+            } else {
+                compArr.push([arr[i]]);
+            }
+        }
+
+        return compArr;
     }
 }
