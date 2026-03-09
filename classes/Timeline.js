5a90800e32b9d255519b855accb9c8e7903614de
QueerCodedDev <120541616+QueerCodedDev@users.noreply.github.com>
2026-03-09 18:58:44 +0000

    Testing the output of the first two compressed entries before moving further

--- classes/Timeline.js
+++ classes/Timeline.js
@@ -2,5 +2,8 @@ class Timeline {
     constructor(dataJSON) {
         this.dataEM = new DataEntryManager(dataJSON.media);
         this.compEM = new CompressedEntryManager(this.dataEM);
+
+        this.compEM.compEntries[0].render();
+        this.compEM.compEntries[1].render();
     }
 }
---
+++ classes/entries/CompressedEntry.js
@@ -0,0 +1,22 @@
+class CompressedEntry {
+    constructor(entry) {
+        this.prev;
+        this.next;
+
+        this.entry  = entry;
+        this.output = this.formatEntry();
+    }
+
+    formatEntry() {
+        let output = '';
+        for (let e of this.entry) {
+            output += `${e.title}\n${e.date}\n`
+        }
+
+        return output;
+    }
+
+    render() {
+        console.log(this.output)
+    }
+}
