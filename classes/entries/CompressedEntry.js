86adc64e0092d40a6d43ef6c85622a9b293a178d
QueerCodedDev <120541616+QueerCodedDev@users.noreply.github.com>
2026-03-09 19:27:08 +0000

    changed stroke to white.

--- classes/entries/CompressedEntry.js
+++ classes/entries/CompressedEntry.js
@@ -17,6 +17,7 @@ class CompressedEntry {
     }
 
     render() {
+        stroke(255)
         rect(0, 0, textWidth(this.output), textAscent() * this.entry.length * 2);
         text(this.output, 0, 0);
     }
