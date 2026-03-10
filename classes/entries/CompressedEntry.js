b6ae64f36fee2f13fc8a47097aab6051c6cf956f
QueerCodedDev <120541616+QueerCodedDev@users.noreply.github.com>
2026-03-10 18:00:47 +0000

    typo in .length that prevented calculation

--- classes/entries/CompressedEntry.js
+++ classes/entries/CompressedEntry.js
@@ -25,7 +25,7 @@ class CompressedEntry {
 
         // let height = (textAscent() + textDescent()) * this.entry.length * 2;
         // height += textLeading() * (this.entry.length-1);
-        let height = textLeading() * ((this.entry.lenth*2) - 1) + textSize();
+        let height = textLeading() * ((this.entry.length*2) - 1) + textSize();
 
         strokeWeight(5);
         stroke(ColorManager.colors[this.entry[0].universe]);
