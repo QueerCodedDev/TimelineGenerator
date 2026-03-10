d62aeadd0711dd35a26071713b382bfc03271329
QueerCodedDev <120541616+QueerCodedDev@users.noreply.github.com>
2026-03-10 16:12:14 +0000

    Removed *2 from height portion of rect rendering

--- classes/entries/CompressedEntry.js
+++ classes/entries/CompressedEntry.js
@@ -28,7 +28,7 @@ class CompressedEntry {
         strokeWeight(5);
         stroke(ColorManager.colors[this.entry[0].universe]);
         fill(ColorManager.colors.black);
-        rect(0, 0, textWidth(this.output), height * (this.entry.length) * 2, 25); // <-- Change height bs here to use textLeading()
+        rect(0, 0, textWidth(this.output), height * (this.entry.length), 25); // <-- Change height bs here to use textLeading()
         
         fill(ColorManager.colors.white)
         stroke(ColorManager.colors.black);
