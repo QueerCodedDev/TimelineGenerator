fcb5e453989d1b605010299bfb91683ad6f6143a
QueerCodedDev <120541616+QueerCodedDev@users.noreply.github.com>
2026-03-10 19:50:15 +0000

    The issue was I was passing in an object instead of a string. Fixed now.

--- classes/entries/CompressedEntry.js
+++ classes/entries/CompressedEntry.js
@@ -27,7 +27,7 @@ class CompressedEntry {
         textAlign(CENTER, CENTER);
         textSize(25);
 
-        let bounds = font.textBounds(this.output, 0, 0, textSize());
+        let bounds = font.textBounds(this.output.titles, 0, 0, textSize());
         let height = bounds.h
         console.log(bounds);
 
