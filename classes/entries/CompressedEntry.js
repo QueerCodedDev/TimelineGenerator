961083ab892d51c3626e494318e43151dcf1cf43
QueerCodedDev <120541616+QueerCodedDev@users.noreply.github.com>
2026-03-10 18:43:52 +0000

    Changing height calculation again

--- classes/entries/CompressedEntry.js
+++ classes/entries/CompressedEntry.js
@@ -23,7 +23,7 @@ class CompressedEntry {
         textSize(25);
         
 
-        let height = textSize() * this.entry.length * 2.5
+        let height = textSize() * this.entry.length * 3
 
         strokeWeight(5);
         stroke(ColorManager.colors[this.entry[0].universe]);
