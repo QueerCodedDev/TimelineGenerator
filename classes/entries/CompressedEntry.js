3f0c0f5e5cb64f524dd6cf472c3f900e8514ddf5
QueerCodedDev <120541616+QueerCodedDev@users.noreply.github.com>
2026-03-10 19:59:45 +0000

    Adjusting height by textSize*3 to account for the date that needs to be included

--- classes/entries/CompressedEntry.js
+++ classes/entries/CompressedEntry.js
@@ -28,7 +28,7 @@ class CompressedEntry {
         textSize(25);
 
         let bounds = font.textBounds(this.output.titles, 0, 0, textSize());
-        let height = bounds.h + (textSize() * 2);
+        let height = bounds.h + (textSize() * 3);
 
         strokeWeight(5);
         stroke(ColorManager.colors[this.entry[0].universe]);
