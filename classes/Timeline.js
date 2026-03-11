4611969043315f526b193b52227ae7ebfa221770
QueerCodedDev <120541616+QueerCodedDev@users.noreply.github.com>
2026-03-11 19:36:03 +0000

    Added autoScroll to Timeline class

--- classes/Timeline.js
+++ classes/Timeline.js
@@ -3,14 +3,22 @@ class Timeline {
         // Configure rect render and text render settings
         rectMode(CENTER);
         textAlign(CENTER);
-        this.dataEM   = new DataEntryManager(dataJSON.media);
-        this.compEM   = new CompressedEntryManager(this.dataEM);
+        this.dataEM = new DataEntryManager(dataJSON.media);
+        this.compEM = new CompressedEntryManager(this.dataEM);
+
+        this.scrollInc = 500;
+        this.scrollAmt = 0;
+    }
+
+    autoScroll() {
+        if (this.scrollAmt <= -this.scrollInc) translate(0, this.scrollAmt);
+        this.scrollAmt -= this.scrollInc;
     }
 
     render() {
         stroke(Renderer.grey);
         strokeWeight(Renderer.rectWeight);
-        line(0, 0, 0, canvasH * 1.75)
+        line(0, 0, 0, canvasH * 2); // *2 is larger than what is needed, but its fine
         this.compEM.render();
     }
 }
--- sketch.js
+++ sketch.js
@@ -9,9 +9,6 @@ const SORT_OPTIONS = {
 let canvasW = 1000;
 let canvasH = 50000
 
-let i = 0;
-let inc = 500;
-
 function preload() {
     dataJSON = loadJSON('res/individual_data.json');
     font = loadFont('res/Consolas.ttf');
@@ -27,7 +24,6 @@ function draw() {
     background(Renderer.darkGrey); // <--- Need to draw a background when autoscrolling
     // Move origin so that it is in the middle, and 20 pixels from the top
     translate(canvasW / 2, Renderer.offset); // <--- translate needs to be in draw
-    if (i <= -inc) translate(0, i);
+    timeline.autoScroll();
     timeline.render();
-    i -= inc;
 }
