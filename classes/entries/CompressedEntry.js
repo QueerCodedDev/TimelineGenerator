ea2b3dd1970dc579e2f5ecb562753d3781839735
QueerCodedDev <120541616+QueerCodedDev@users.noreply.github.com>
2026-03-11 16:33:46 +0000

    Changed height and width to be this.h and this.w

--- classes/entries/CompressedEntry.js
+++ classes/entries/CompressedEntry.js
@@ -4,6 +4,8 @@ class CompressedEntry {
         this.next;
 
         this.entry  = entry;
+        this.w;
+        this.h;
         this.output = this.formatEntry();
     }
 
@@ -27,20 +29,20 @@ class CompressedEntry {
         textAlign(CENTER, CENTER);
         textSize(Renderer.point);
 
-        let bounds = font.textBounds(this.output.titles, 0, 0, Renderer.point);
-        let dateBounds = font.textBounds(this.output.dates, 0, 0, Renderer.point);
+        let textBounds = font.textBounds(this.output.titles, 0, 0, Renderer.point);
+        let dateBounds = font.textBounds(this.output.dates,  0, 0, Renderer.point);
 
-        let height = max(bounds.h, dateBounds.h) + (Renderer.point * 3);
-        let width  = max(bounds.w, dateBounds.w) + Renderer.point;
+        this.h = max(textBounds.h, dateBounds.h) + (Renderer.point * 3);
+        this.w = max(textBounds.w, dateBounds.w) +  Renderer.point;
 
-        strokeWeight(5);
+        strokeWeight(Renderer.rectWeight);
         stroke(ColorManager.colors[this.entry[0].universe]);
         fill(ColorManager.colors.black);
-        rect(0, 0, width, height, 25);
+        rect(0, 0, this.w, this.h, 25);
         
         fill(ColorManager.colors.white)
         stroke(ColorManager.colors.black);
-        strokeWeight(2);
+        strokeWeight(Renderer.textWeight);
         text(this.output.titles, 0, 0);
         fill(ColorManager.colors.line)
         text(this.output.dates, 0, 0);
@@ -48,6 +50,6 @@ class CompressedEntry {
         textSize(Renderer._point);
         noStroke();
         fill(ColorManager.colors[this.entry[0].universe]);
-        text(this.entry[0].name, 0, (-height/2) + Renderer._point);
+        text(this.entry[0].name, 0, (-this.h/2) + Renderer._point);
     }
 }
